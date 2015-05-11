649c5d5ddc1eadfea457328b409fa01dd872f97c
Papp Krisztián <tacsiazuma@gmail.com>
2015-05-12 00:53:14 +0200

    Project skeleton added.

--- test.js
+++ test.js
@@ -3,8 +3,9 @@
  */
 var assert = require('assert');
 
-var bot = require("./index.js").bot();
+var bot = require("./index.js").core();
 var expect = require("chai").expect;
+var http = require("http");
 
 describe('Bot', function(){
     describe('#bot()', function(){
@@ -25,6 +26,27 @@ describe('Bot', function(){
     describe('#endpoints', function(){
         it('should be an object', function(){
             expect(bot.endpoints).to.be.object;
+        })
+    })
+    describe('#server', function(){
+        it('should listen on 9091', function(){
+            // bot.server(); // start the server
+            options = {
+                host: "http://localhost",
+                port: 9091,
+                method: "POST"
+            };
+            bot.start(); // start listening
+            var req = http.request(options);
+
+            req.on("response",function(response) {
+                this.expect(response.statusCode).to.equal(200);
+            }, this);
+
+            req.end(); // send a request
+
+
+            // bot.stop();
         })
     })
 })
